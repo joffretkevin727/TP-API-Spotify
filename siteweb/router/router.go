cd2962e007bfe38b6fb5f736b614dc634c886792
mac boulanger puget <macboulangerpuget@MacBook-Pro-de-mac.local>
2025-11-21 13:07:57 +0100

    style: create first style for albumdamso.html

--- siteweb/router/router.go
+++ siteweb/router/router.go
@@ -12,6 +12,11 @@ func New() *http.ServeMux {
 	mux.HandleFunc("/home", controller.Home)
 	mux.HandleFunc("/album/damso", controller.AlbumDamso)
 	mux.HandleFunc("/tracks/laylow", controller.TracksLaylow)
+
+	// --- STATIC FILES ---
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
 	//--- ROUTES ---
 	return mux
 }
