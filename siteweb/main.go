1bff46ead759c95b7ed660f11754815dbab04157
Kevin <joffretkevin727@gmail.com>
2025-11-21 21:16:58 +0100

    feature: web page trackslaylow.html functionnal

--- siteweb/main.go
+++ siteweb/main.go
@@ -7,9 +7,12 @@ import (
 	"net/http"
 )
 
+// FONCTION PRINCIPAL
 func main() {
 	api.GetToken()
+	//liens utile au TP
 	//"https://api.spotify.com/v1/artists/2UwqpfQtNuhBwviIC0f2ie/albums"
+	//"https://api.spotify.com/v1/tracks/0nAHBAlzkyaQXUp7qTULqv"
 
 	r := router.New()
 	fmt.Println("http://localhost:8080/home")
